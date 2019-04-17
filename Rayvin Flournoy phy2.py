d9685bb8914eb5e048cf285afa9afbdfcb0cd701
flournor <49569228+flournor@users.noreply.github.com>
2019-04-17 09:13:38 -0400

    first submission

---
+++ Rayvin Flournoy phy2.py
@@ -0,0 +1,8 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: E:/P2HW1_POUNDSKILOGRAMS_rAYVINfLOURNOY.PY ============
+how many pounds do you want to convert?10
+the total of kg enetered is 4.54
+the total of lb entered is 10.00
+>>> 
